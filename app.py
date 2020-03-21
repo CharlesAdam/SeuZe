c28a865ebe6a7ac8689b9bb82485978787a8a33c
Victor <v.pasetto96@gmail.com>
2020-03-21 13:29:36 -0300

    base structure for function created

--- app.py
+++ app.py
@@ -1,10 +1,69 @@
-from flask import Flask
-app = Flask(__name__)
+import sys
+import logging
+import rds_config
+import psycopg2
 
-@app.route('/')
-def hello_world():
-    return 'Hey, we have Flask in a Docker container!'
+#rds settings
+rds_host  = rds_config.rds_host
+name = rds_config.db_username
+password = rds_config.db_password
+db_name = rds_config.db_name
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+try:
+    conn = psycopg2.connect(host=rds_host, user=name, password=password, database=db_name, connect_timeout=5)
+except psycopg2.Error as e:
+    logger.error("ERROR: Unexpected error: Could not connect to PostgreSQL instance.")
+    logger.error(e)
+    sys.exit()
+
+logger.info("SUCCESS: Connection to RDS PostgreSQL instance succeeded")
+def lambda_handler():
+    """
+    This function fetches content from PostgreSQL RDS instance
+    """
+
+    item_count = 0
+
+    with conn.cursor() as cur:
+        cur.execute(
+            '''INSERT INTO ft_report_brazil_city_dairy(
+                datasource,
+                city,
+                state,
+                uid,
+                suspects,
+                refuses,
+                cases,
+                casesnew,
+                deaths,
+                deathsnew,
+                comments,
+                broadcast
+            ) values (
+                'S3',
+                'SP',
+                'SP',
+                1,
+                0,
+                0,
+                0,
+                1,
+                1,
+                1,
+                'teste comentario',
+                B'1'
+                )''')
+        conn.commit()
+        cur.execute("select * from ft_report_brazil_city_dairy")
+        for row in cur:
+            item_count += 1
+            logger.info(row)
+            print(row)
+    conn.commit()
+
+    return "Added %d items from RDS PostgreSQL table" %(item_count)
+
+lambda_handler()
---
+++ rds_config.py
@@ -0,0 +1,5 @@
+#config file containing credentials for RDS MySQL instance
+db_username = "username"
+db_password = ""
+db_name = "" 
+rds_host  = ""
