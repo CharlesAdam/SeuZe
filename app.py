1eb57a74e92d438a5d706b6ecf0c3c2e2bd8f20d
Victor <v.pasetto96@gmail.com>
2020-03-21 15:27:37 -0300

    brasilIO conversion

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import sys
 import logging
 import rds_config
 import psycopg2
+import json
+from types import SimpleNamespace as Namespace
 
 #rds settings
 rds_host  = rds_config.rds_host
@@ -24,46 +26,46 @@ def lambda_handler():
     """
     This function fetches content from PostgreSQL RDS instance
     """
-
+    payload = '''{"count":291,"next":null,"previous":null,"results":[{"city":"Laranjal do Jarí","confirmed":0,"date":"2020-03-20","deaths":null,"discarded":null,"notes":null,"notified":null,"place_type":"city","source_url":"https://www.portal.ap.gov.br/noticia/2003/boletim-informativo-covid-19-amapa-20-de-marco-de-2020","state":"AP","suspect":2},{"city":"Macapá","confirmed":1,"date":"2020-03-20","deaths":null,"discarded":null,"notes":null,"notified":null,"place_type":"city","source_url":"https://www.portal.ap.gov.br/noticia/2003/coronavirus-primeiro-caso-e-confirmado-no-amapa","state":"AP","suspect":21},{"city":"Pedra Branca do Amapari","confirmed":0,"date":"2020-03-20","deaths":null,"discarded":null,"notes":null,"notified":null,"place_type":"city","source_url":"https://www.portal.ap.gov.br/noticia/2003/boletim-informativo-covid-19-amapa-20-de-marco-de-2020","state":"AP","suspect":1}]}'''
+    report = json.loads(payload, object_hook=lambda d: Namespace(**d))
     item_count = 0
-
     with conn.cursor() as cur:
-        cur.execute(
+        for case in report.results:
+            if(case.place_type == "city"):
+                add_to_table(cur, case)
+    
+
+    return "Added %d items from RDS PostgreSQL table" %(item_count)
+
+
+def add_to_table(cur, report):
+    data = (report.city, report.state, report.deaths, report.discarded, report.confirmed, report.suspect, report.notes)
+    cur.execute(
             '''INSERT INTO ft_report_brazil_city_dairy(
                 datasource,
                 city,
                 state,
-                uid,
-                suspects,
+                deaths,
                 refuses,
                 cases,
-                casesnew,
-                deaths,
-                deathsnew,
-                comments,
-                broadcast
+                suspects,
+                comments
             ) values (
-                'S3',
-                'SP',
-                'SP',
-                1,
-                0,
-                0,
-                0,
-                1,
-                1,
-                1,
-                'teste comentario',
-                B'1'
-                )''')
-        conn.commit()
-        cur.execute("select * from ft_report_brazil_city_dairy")
-        for row in cur:
-            item_count += 1
-            logger.info(row)
-            print(row)
+                'brasilio-bucket',
+                %s,
+                %s,
+                %s,
+                %s,
+                %s,
+                %s,
+                %s
+                )''', data)
+    conn.commit()
+    cur.execute("select * from ft_report_brazil_city_dairy")
+    for row in cur:
+        logger.info(row)
+        print(row)
     conn.commit()
 
-    return "Added %d items from RDS PostgreSQL table" %(item_count)
 
 lambda_handler()
